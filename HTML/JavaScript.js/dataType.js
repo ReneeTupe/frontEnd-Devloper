6f4c18f2654803d3c8793fb272ffb89bbb6e45ed
ReneeTupe <reneetupe@gmail.com>
2026-01-18 23:43:39 -0800

    task of js

---
+++ HTML/JavaScript.js/dataType.js
@@ -0,0 +1,16 @@
+var name = 'Renee';
+
+var surname = 'Tupe';
+
+var maried = 'false';
+
+var age = '17';
+
+var ans = '${name} ${surname} ${maried} ${age}';
+
+for(let i=0;i<3;i++){
+    console.log(i)
+}
+let val=3;
+val='Tupe';
+console.log(val);
