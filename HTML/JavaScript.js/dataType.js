2d0249749404ee176182a1443ee18055a7b239bd
ReneeTupe <reneetupe@gmail.com>
2026-01-21 00:47:59 -0800

    code

--- HTML/JavaScript.js/dataType.js
+++ HTML/JavaScript.js/dataType.js
@@ -16,3 +16,52 @@ val='Tupe';
 console.log(val);
 
 //hello
+
+// primitive - [call by vale] [point to memory]
+
+/**
+ * 
+ * ! string - "" '' ``
+ * # number - 0-9
+ * $ boolean - true/false
+ * * symbol - symbol()
+ * & undefined - undefined
+ * ? null - null 
+ * bigint - 35245543759757.n
+ * 
+ * 
+ * ! array - []
+ * # object - {}
+ * $ function - ()
+ * *
+ */
+
+let name1 = 'shivam';
+//console.log(~ name:' name);
+
+name1 = 'Renee';
+// console.log(~name:' name);
+
+name1[0] = 'S';
+console.log( 'name:',name1);
+console.log( 'name[0]:',name1[0]);
+
+let othername = 'shivam';
+
+let arr = [
+    1,
+    3,
+    4,
+    name1,
+    othername,
+    'Renee',
+    [true,false],
+    {name: 'mehfooz', age: 99},
+    ];
+
+    arr[0] = 'sneha';
+    arr[1] = 'abhishek';
+
+    console.log('~ arr[0]:',arr[0]); // sneha
+    console.log('~ arr:', arr);
+
